d36153d55fbb5f2c583d408db964ac74c05e036d
松田 翔大 <thank2626@gmail.com>
2018-04-28 22:33:46 +0900

    ファイル追加

---
+++ CustomTextView/CustomTextView/CustomTextView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTextView.swift
+//  CustomTextView
+//
+//  Created by 松田翔大 on 2018/04/28.
+//  Copyright © 2018年 松田翔大. All rights reserved.
+//
+
+import UIKit
+
+public class CustomTextView: UITextView {
+
+}
